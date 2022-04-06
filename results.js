c45899974f175240fecabd163404102f41696844
nomboibrahim <nomboibrahim87@gmail.com>
2022-04-06 07:54:22 -0700

    first commit

---
+++ results.js
@@ -0,0 +1,15 @@
+const results =[
+{name:"Windy Dari", class:"Form One", marks:8},
+{name:"Zakaria Ab", class:"Form One", marks:6},
+{name:"Flex Dass", class:"Form One", marks:2},
+{name:"Awudu Dery", class:"Form One", marks:10},
+{name:"Dady Ama", class:"Form One", marks:7},
+{name:"Kofi Addo", class:"Form One", marks:5},
+{name:"Asamoah", class:"Form One", marks:9},
+{name:"Osei Ama", class:"Form One", marks:3},
+{name:"Micheal", class:"Form One", marks:7},
+{name:"Yaw", class:"Form One", marks:7},
+
+]
+
+module.exports=results
